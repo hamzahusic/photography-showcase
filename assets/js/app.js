dd49fc30514223da0dfb983e3edb652ce0dd27dd
Hamza Husic <hamzahusic.dev@gmail.com>
2025-01-07 15:46:19 +0100

    feature: Implemented  highlighting the active page on navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,12 @@ const mobile_menu = document.getElementsByClassName("mobile-menu-links")[0]
 menu_btn.addEventListener("click", ()=>{
    mobile_menu.classList.toggle("active") 
 })
+
+const desktop_menu_links = document.querySelectorAll(".nav-links a")
+
+desktop_menu_links.forEach(link => {
+   link.onclick = () => {
+      desktop_menu_links.forEach(mlink => mlink.classList.remove('active-link'))
+      link.classList.add('active-link')
+   }
+})
